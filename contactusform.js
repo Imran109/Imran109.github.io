42c1d05e72f887932e4336fc0e873e119e8bb867
Imran109 <42968710+Imran109@users.noreply.github.com>
2018-12-22 10:42:34 +0530

    Create contactusform.js

---
+++ contactusform.js
@@ -0,0 +1,42 @@
+let form=document.getElementById("fform");
+form.addEventListener("submit",event=>{
+   let user=form.elements["Name"];
+   let userError=document.getElementById("user-errorr");
+   let msg=form.elements["Message"];
+   let msgError=document.getElementById("msg");
+   let email=form.elements["Email"];
+   let emailError=document.getElementById("emailerror");
+   if(user.value.length <5){
+    event.preventDefault();
+   userError.textContent="Invalid entry-minimum 5 characters";
+   userError.style.color="blue";
+   user.style.borderColour="yellow";
+   user.focus()
+   }
+
+    if(email.value.length<7){
+    event.preventDefault();
+    user.focus()
+    emailError.textContent="Please enter a valid Email ID";
+    emailError.style.color="blue";
+   }
+
+    if(msg.value.length<1){
+    event.preventDefault();
+    user.focus()
+    msgError.textContent="Message field cannot be Blank";
+    msgError.style.color="blue";
+   }
+
+   if(user.value.length>5)
+   {
+    userError.textContent=" ";
+   }
+   if(msg.value.length>1){
+    msgError.textContent=" ";
+   }
+   if(email.value.length>7){
+    emailError.textContent=" ";
+   }
+});
+
