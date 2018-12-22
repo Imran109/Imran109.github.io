44bbc7a4af8d125801115f844ee9c9fdd9422e43
Imran109 <42968710+Imran109@users.noreply.github.com>
2018-12-22 11:08:55 +0530

    Update formm.js

--- formm.js
+++ formm.js
@@ -4,14 +4,14 @@ form.addEventListener("submit",event=>{
    let userError=document.getElementById("user-error");
    let email=form.elements["Email"];
    let emailError=document.getElementById("emailerror");
-   if(user.value.length < 5){
+   if(user.value.length < 4){
     event.preventDefault();
    userError.textContent="Invalid entry-minimum 5 characters";
    userError.style.color="green";
    user.style.borderColour="yellow";
    user.focus();
    }
-   if((user.value.length>5)&&user.value!=="imranps"){
+   if((user.value.length>4)&&(user.value!=="imranps"||user.value!=="S Priya")){
     userError.textContent="Access denied.Contact P.S.M.I to gain access";
     event.preventDefault();
     user.focus();
@@ -26,7 +26,7 @@ form.addEventListener("submit",event=>{
      if(email.value.length>7){
       emailError.textContent=" ";
      }
-     if(user.value==="imranps"){
+     if(user.value==="imranps" || user.value==="S Priya"){
       userError.textContent=" ";
      }
 });
