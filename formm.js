eaf12d6209559f30747199b3797ff4d3454d444e
Imran109 <42968710+Imran109@users.noreply.github.com>
2018-12-22 10:42:30 +0530

    Create formm.js

---
+++ formm.js
@@ -0,0 +1,35 @@
+let form=document.getElementById("fform");
+form.addEventListener("submit",event=>{
+   let user=form.elements["Name"];
+   let userError=document.getElementById("user-error");
+   let email=form.elements["Email"];
+   let emailError=document.getElementById("emailerror");
+   if(user.value.length < 5){
+    event.preventDefault();
+   userError.textContent="Invalid entry-minimum 5 characters";
+   userError.style.color="green";
+   user.style.borderColour="yellow";
+   user.focus();
+   }
+   if((user.value.length>5)&&user.value!=="imranps"){
+    userError.textContent="Access denied.Contact P.S.M.I to gain access";
+    event.preventDefault();
+    user.focus();
+   }
+
+    if(email.value.length<7){
+      event.preventDefault();
+      user.focus()
+      emailError.textContent="Please enter a valid Email ID";
+      emailError.style.color="blue";
+     }
+     if(email.value.length>7){
+      emailError.textContent=" ";
+     }
+     if(user.value==="imranps"){
+      userError.textContent=" ";
+     }
+});
+
+
+
